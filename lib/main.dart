f61bb4f24e3dbaa5ce114a991ea593c69576f067
unknown <mh14021999@gmail.com>
2024-07-06 19:36:36 +0500

    prescription and report screen in doctor section completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pashusevak/firebase_options.dart';
 import 'package:pashusevak/screens/appointmentDetails.dart';
+import 'package:pashusevak/screens/doctorHome.dart';
 import 'package:pashusevak/screens/emergencyAppointment.dart';
+import 'package:pashusevak/screens/prescriptionAndReport.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const EmergencyAppointment(),
+      home: const PrescriptionAndReport(),
     );
   }
 }
